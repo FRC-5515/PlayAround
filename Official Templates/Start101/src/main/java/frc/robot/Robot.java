7ecb6621bdc943af2e094128e087f7424e820953
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-01-19 17:55:00 +0800

    Template1Getting Started

--- Official Templates/Start101/src/main/java/frc/robot/Robot.java
+++ Official Templates/Start101/src/main/java/frc/robot/Robot.java
@@ -5,200 +5,76 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team5515.robot;
+package frc.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.PWM;
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-import com.ctre.phoenix.motorcontrol.can.*;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.DigitalInput;
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
+ * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
  */
-public class Robot extends IterativeRobot {
-	private static final String kDefaultAuto = "Default";
-	private static final String kCustomAuto = "My Auto";
-	private String m_autoSelected;
-	private SendableChooser<String> m_chooser = new SendableChooser<>();
-	private Solenoid intakeSolenoid = new Solenoid(1);
+public class Robot extends TimedRobot {
+  private final DifferentialDrive m_robotDrive
+      = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+  private final Joystick m_stick = new Joystick(0);
+  private final Timer m_timer = new Timer();
 
-	private Joystick Oscar = new Joystick(0);
-	private Joystick Myra = new Joystick(1);
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+  }
 
-	private WPI_TalonSRX elevator = new WPI_TalonSRX(5);
-	
-	private WPI_TalonSRX leftmaster = new WPI_TalonSRX(2); //leftmaster
-	private WPI_TalonSRX leftmaster1 = new WPI_TalonSRX(8);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-	private WPI_TalonSRX rightmaster = new WPI_TalonSRX(3); //rightmaster
-	
-	//private WPI_TalonSRX rightmaster1 = new WPI_TalonSRX(7);
-	//private DifferentialDrive BP19=new DifferentialDrive(leftmaster,rightmaster);
-	//private DifferentialDrive BP20=new DifferentialDrive(leftmaster1,rightmaster1);
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		m_chooser.addDefault("Default Auto", kDefaultAuto);
-		m_chooser.addObject("My Auto", kCustomAuto);
-		SmartDashboard.putData("Auto choices", m_chooser);
-		
-		/* get the decoded pulse width encoder position, 4096 units per rotation 
-		int pulseWidthPos = rightmaster.getSensorCollection().getPulseWidthPosition(); 
-		/* get the pulse width in us, rise-to-fall in microseconds 
-		int pulseWidthUs = rightmaster.getSensorCollection().getPulseWidthRiseToFallUs(); 
-		/* get the period in us, rise-to-rise in microseconds 
-		int periodUs = rightmaster.getSensorCollection().getPulseWidthRiseToRiseUs(); 
-		/* get measured velocity in units per 100ms, 4096 units is one rotation 
-		int pulseWidthVel = rightmaster.getSensorCollection().getPulseWidthVelocity(); 
-		/* is sensor plugged in to Talon
-		boolean sensorPluggedIn = false; 
-		if (periodUs != 0) { 
-			sensorPluggedIn = true;
-		}
-		int pulseWidthPoss = rightmaster1.getSensorCollection().getPulseWidthPosition(); 
-	 	get the pulse width in us, rise-to-fall in microseconds 
-		int pulseWidthUss = rightmaster1.getSensorCollection().getPulseWidthRiseToFallUs(); 
-		 get the period in us, rise-to-rise in microseconds 
-		int periodUss = rightmaster1.getSensorCollection().getPulseWidthRiseToRiseUs(); 
-		 get measured velocity in units per 100ms, 4096 units is one rotation 
-		int pulseWidthVell = rightmaster1.getSensorCollection().getPulseWidthVelocity(); 
-		is sensor plugged in to Talon 
-		boolean sensorPluggedInn = false; 
-		if (periodUs != 0) { 
-			sensorPluggedInn = true;
-		}
-		*/
-		rightmaster.set(ControlMode.PercentOutput, 0);
-		//rightmaster1.set(ControlMode.PercentOutput,0);
-		rightmaster.setInverted(true);
-		//rightmaster1.setInverted(true);
+  /**
+   * This function is run once each time the robot enters autonomous mode.
+   */
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
 
-		
-		elevator.set(ControlMode.PercentOutput, 0);
-		elevator.setInverted(true);
-	}
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+    } else {
+      m_robotDrive.stopMotor(); // stop robot
+    }
+  }
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + m_autoSelected);
-	}
+  /**
+   * This function is called once each time the robot enters teleoperated mode.
+   */
+  @Override
+  public void teleopInit() {
+  }
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-			case kCustomAuto:
-				// Put custom auto code here
-				break;
-			case kDefaultAuto:
-			default:
-				// Put default auto code here
-				break;
-		}
-	}
+  /**
+   * This function is called periodically during teleoperated mode.
+   */
+  @Override
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+  }
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	@Override
-	public void teleopPeriodic() {
-		if(Myra.getRawButton(7)) {
-		double turn=Oscar.getRawAxis(0);
-		double forward=Oscar.getRawAxis(1);
-		//BP19.arcadeDrive(-0.9*forward,0.8*turn);
-		//BP20.arcadeDrive(-0.9*forward,0.8*turn);
-		}
-		else if(Myra.getRawButton(8)) {
-			double turn=Oscar.getRawAxis(0);
-			double forward=Oscar.getRawAxis(1);
-			//BP19.arcadeDrive(0.9*forward,-0.8*turn);
-			//BP20.arcadeDrive(0.9*forward,-0.8*turn);
-		}
-		else{
-			double turn=Oscar.getRawAxis(0);
-			double forward=Oscar.getRawAxis(1);
-			//BP19.arcadeDrive(-0.9*forward,0.8*turn);
-			//BP20.arcadeDrive(-0.9*forward,0.8*turn);
-			}
-		if(Oscar.getRawButton(2)) {
-		intakeSolenoid.set(true);
-		}
-		if(Oscar.getRawButton(1)) {
-			intakeSolenoid.set(false);
-		}
-		//double elevator=Myra.getRawAxis(1);
-		
-		
-		
-		
-		
-		/*if (Oscar.getRawButton(7)) {
-			elevator.set(1);
-		}
-		if(Myra.getRawButton(4)) {
-			elevator.set(-0.5);
-		}*/
-		if(Oscar.getRawButton(12)) {
-		rightmaster.set(0.3);
-		rightmaster.configForwardSoftLimitThreshold(30*4096, 10);/* +14 rotations forward when using CTRE Mag encoder */ 
-		rightmaster.configForwardSoftLimitEnable(true, 10); 
-		rightmaster.overrideLimitSwitchesEnable(true);
-		}
-		else {
-			rightmaster.set(0.3);
-			rightmaster.configForwardSoftLimitThreshold(0*4096, 10);/* +14 rotations forward when using CTRE Mag encoder */ 
-			rightmaster.configForwardSoftLimitEnable(true, 10); 
-			rightmaster.overrideLimitSwitchesEnable(true);
-			}
-		//rightmaster1.configForwardSoftLimitThreshold(+14*4096, 10);/* +14 rotations forward when using CTRE Mag encoder */ 
-		//rightmaster1.configForwardSoftLimitEnable(true, 10); 
-		//rightmaster1.overrideLimitSwitchesEnable(true);
-		
-		}
-	
-
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
 }
